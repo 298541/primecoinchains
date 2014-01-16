2bfa5dc1ea556ef2c279056e2b8e91a95732796c
lacksfish <lacksfish@gmail.com>
2014-01-16 17:31:36 +0100

    Update footer.php
    Updated wallet addresses. Ready for donations. :P

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
         <div style="text-align: center;">
             <p><a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#108;&#97;&#99;&#107;&#115;&#102;&#105;&#115;&#104;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;">Contact</a></p>
             <p><b>Student piggy bank</b><br>
-            XPM: AccoQkyLNXYsheDiDy9Q5e6qv1VP4CN7ux<br>
+            XPM: AUoCLdyDNmXgKFgiQgkxJhL8rq6mixvX9R<br>
             PPC: PFTBKAPg3YwZRfWXZhJ6f7yXcS1V5Hcieq<br>	
-            BTC: 12mJujuKGb3nxgySGgrH81HvjXH5ZDw1JU</p>
+            BTC: 1383CyywThWrGvyUwdPxPan4R1QMrg4cJF</p>
             <p><small>All primechains are based on the primecoin blockchain and are provided without waranty of correctness</small>
             <br>
             <p><small><b>&copy; 20XX - primecoinchains</b></small></p>
